d71904128f0b925c0ba13ce2a05007ade39fb15c
Diego Montemor <montemordiego@gmail.com>
2025-08-19 19:15:34 -0300

    Adiciona funções de manipulação de registros e menu interativo no programa

--- aula1.cpp
+++ aula1.cpp
@@ -6,6 +6,19 @@ typedef struct {
     int chave;
 } Registro;
 
+int menu(){
+    int op;
+    cout << "1. Busca1\n"
+         << "2. Busca2\n"
+         << "3. Busca Ordena\n"
+         << "4. Busca Binaria\n"
+         << "5. Incercerir\n"
+         << "6. remover\n"
+         << "R: ";
+    cin >> op;
+    return op;
+}
+
 int busca(Registro L[], int x , int n){
     int i = 0 , 
         pos = 0;
@@ -62,27 +75,72 @@ int buscaBinaria(Registro L[], int x, int n){
     return buscaBin + 1;
 }
 
+int inserir(Registro L[], int QuantAtual, int Quant, int x){
+    if(QuantAtual <= Quant){
+        if(busca2(L, x, QuantAtual) == 0){
+            L[QuantAtual + 1].chave = x;
+            QuantAtual++;
+            return QuantAtual;
+        }else return 0;
+    }else return -1;
+}
+
+int* remover(Registro L[], int QuantAtual, int Quant, int x){
+    if(QuantAtual >= 0){
+        
+    }else return nullptr;
+}
 
 int main() {
     Registro L[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int b1, b2, bo, bb;
-    cout << "Digite um numero para buscar pela funcao busca1: ";
-    cin >> b1;
-    b1 = busca(L,b1,10);
-    cout << "Digite um numero para buscar pela funcao busca2: ";
-    cin >> b2;
-    b2 = busca2(L,b2,10);
-    cout << "Digite um numero para buscar pela funcao busca ordenada: ";
-    cin >> bo;
-    bo = buscaOrd(L,bo,10);
+    int b1, b2, bo, bb, ins, rem, op = 10;
 
-    cout << "Digite um numero para buscar pela funcao busca binaria: ";
-    cin >> bb;
-    bb = buscaBinaria(L, bb, 10);
+    while(op != 0){
+        switch (op){
+        case 1:{
+            cout << "Digite um numero para buscar pela funcao busca1: ";
+            cin >> b1;
+            b1 = busca(L,b1,10);
+            op = menu();
+            break;
+        }
+        case 2:{
+            cout << "Digite um numero para buscar pela funcao busca2: ";
+            cin >> b2;
+            b2 = busca2(L,b2,10);
+            op = menu();
+            break;
+        }
+        case 3:{    
+            cout << "Digite um numero para buscar pela funcao busca ordenada: ";
+            cin >> bo;
+            bo = buscaOrd(L,bo,10);
+            op = menu();
+            break;
+        }
+        case 4:{
+            cout << "Digite um numero para buscar pela funcao busca binaria: ";
+            cin >> bb;
+            bb = buscaBinaria(L, bb, 10);
+            op = menu();
+            break;
+        }
+        case 5:{
+            cout << "Digite um numero para inserir no vetor: ";
+            cin >> ins;
+            ins = inserir(L,10,11,ins);
+            break;
+        }
+        case 6:{
+            cout << "Digite um numero para ser removido do vetor: ";
+            cin >> rem;
+            break;
+        }
+        default:
+            op = menu();   
+            break;
+        }
 
-    cout << "Busca 1: " << b1 << "\nBusca 2: " << b2  << "\nBusca ordenada: " << bo << "\nBusca Binaria: " << bb << endl;
-
-
-
+    }
     return 0;
 }
